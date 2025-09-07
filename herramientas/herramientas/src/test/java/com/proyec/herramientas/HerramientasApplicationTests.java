d3be4dd16df571cb439fc77f1522ae08f5e05219
Luis-026 <U22238714@utp.edu.pe>
2025-09-07 14:16:25 -0500

    Version Base del Proyecto del Curso Herramientas de Desarrollo

---
+++ herramientas/herramientas/src/test/java/com/proyec/herramientas/HerramientasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyec.herramientas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerramientasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
